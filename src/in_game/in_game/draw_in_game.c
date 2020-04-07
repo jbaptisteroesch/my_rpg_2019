77d934666dac40bb245f6e71bfb85a5c0391f819
ROESCH Jean-Baptiste <jean-baptiste.roesch@epitech.eu>
2020-04-07 17:12:24 +0200

    MODIFY draw in game

--- src/in_game/in_game/draw_in_game.c
+++ src/in_game/in_game/draw_in_game.c
@@ -18,7 +18,7 @@ void (*select_draw_in_game[5])(game_t *game) = {
 void draw_in_game(game_t *game)
 {
     select_draw_in_game[game->player.is_on_scene](game);
-    if (game->hit.bool_display == 0)
+    if (game->hit.bool_display == 1)
         drawSprite(WINDOW, game->player.character.sprite, NULL);
     else
         drawSprite(WINDOW, game->player.pl_not_op.sprite, NULL);
